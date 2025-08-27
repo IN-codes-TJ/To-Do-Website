c1fa6b8292e0078e7ce49935c4016facf70620b1
INTJcodes <lolarosegooch@gmail.com>
2025-08-27 13:20:47 +0100

    Querying database, ready to compare credentials
    . Need to create hashing algorithm for the password, then can begin authentication algorithms.

--- login.js
+++ login.js
@@ -6,19 +6,27 @@ document.addEventListener("DOMContentLoaded", () => {
         checkDetails();
     });
 
-    async function checkDetails(){
+    async function checkDetails() {
         //Check details against server here
         try {
           const response = await fetch('/.netlify/functions/check_login');
           const accounts = await response.json();
           
-          accounts.forEach((account) => {
-            console.log(account);
-          });
+          if (accounts.length == 0) {
+            createAccount();
+          }
+          
         } catch (error) {
           console.error('Error:', error);
         }
         //Edit form response if necessary
+    }
+
+    function createAccount() {
+        username = document.getElementById("username").value;
+        password = document.getElementById("password").value;
+        //Hash pasword and put hashed into database
+        console.log(`username: ${username}, and password: ${password}`);
     }
 
 });
--- netlify/functions/check_login/check_login.mjs
+++ netlify/functions/check_login/check_login.mjs
@@ -1,9 +1,9 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
 import { neon } from '@neondatabase/serverless';
 export async function handler(event) {
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon(process.env.NETLIFY_DATABASE_URL);
   try {
-    const rows = await sql('SELECT * FROM accounts;');
+    const rows = await sql`SELECT * FROM accounts;`;
     return {
       statusCode: 200,
       body: JSON.stringify(rows),
